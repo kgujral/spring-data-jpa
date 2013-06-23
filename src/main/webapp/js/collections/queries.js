a9347921321a602288e1568444e034e59fc5d9e5
Karan <kgujral@beachbody.com>
2013-06-24 01:18:55 +0530

    minified files

--- src/main/webapp/js/collections/queries.js
+++
@@ -1,9 +0,0 @@
-define(['backbone', 'models/query', 'localStorage'], function(Backbone, Query, Store) {
-    
-    var Queries = Backbone.Collection.extend({
-    	localStorage: new Store("Query"),
-        model: Query
-    });
-    
-    return Queries;
-});
